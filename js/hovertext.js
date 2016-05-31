15df353c2d283b077a93ce1ef7ab8ebcca9ca9e4
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-31 09:13:34 -0400

    prototype #2 complete !
    nice work, skeleton!

--- js/hovertext.js
+++ js/hovertext.js
@@ -1,3 +1,10 @@
 function csc (tochange) {
-	document.getElementById("centre").innerHTML = "works";
+	var tex;
+	if (tochange=="1") {
+		tex = "asdf1234"
+	} else if (tochange=="2"){
+		tex ="1234asdf"
+	};
+
+	document.getElementById("centre").innerHTML = tex;
 }
