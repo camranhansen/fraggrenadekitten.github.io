ba9d70224dd30ce3b3df4261b838be293951c19e
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-31 09:49:20 -0400

    test

--- js/hovertext.js
+++ js/hovertext.js
@@ -1,10 +1,15 @@
-function csc (tochange) {
-	var tex;
-	if (tochange=="1") {
-		tex = "asdf1234"
-	} else if (tochange=="2"){
-		tex ="1234asdf"
-	};
+// function csc (tochange) {
+// 	var tex;
+// 	if (tochange=="1") {
+// 		tex = "asdf1234"
+// 	} else if (tochange=="2"){
+// 		tex ="1234asdf"
+// 	};
 
-	document.getElementById("centre").innerHTML = tex;
-}
+// 	document.getElementById("centre").innerHTML = tex;
+// }
+
+$(".yellow").on("click", function(e){
+    e.preventDefault();
+    alert('test');
+});
